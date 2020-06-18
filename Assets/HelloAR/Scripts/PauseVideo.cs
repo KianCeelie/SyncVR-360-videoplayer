82725167ec5bf87865cf76520dddada7229b6f53
kian ceelie <kcceelie01@gmail.com>
2020-06-18 19:21:26 +0200

    Commit 1

---
+++ Assets/HelloAR/Scripts/PauseVideo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class PauseVideo : MonoBehaviour
+{
+    private PauseMenu PauseMenu;
+    public VideoPlayer Spherevideo;
+
+    public void Resume()
+    {
+        Spherevideo.playbackSpeed = 1f;
+    }
+
+    public void Pause()
+    { 
+        Spherevideo.playbackSpeed = 0f;
+    }
+
+}
---
+++ Assets/HelloAR/Scripts/menucontrol.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class menucontrol : MonoBehaviour
+{
+
+    public void Quitgame()
+    {
+        // zorft ervoor dat je het spel kan eindigen
+        Application.Quit();
+    }
+
+    public void loadscene(string level)
+    {
+        // dit laad de scenes als je dat oproept
+        SceneManager.LoadScene(level);
+
+    }
+
+    public void LoadMenu()
+    {
+        // laat het startscherm op het pause scherm
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("startscherm");
+    }
+}
